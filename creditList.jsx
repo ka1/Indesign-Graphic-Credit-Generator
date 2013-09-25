68c58b1e6488586622c74e7ce1f901ba110fa0d2
ka1 <github@kasugai.de>
2013-09-25 14:55:51 +0200

    adding a linebreak after list of figures to avoid errors in zotero import script

--- creditList.jsx
+++ creditList.jsx
@@ -386,6 +386,9 @@ function main(){
 		myDocument.hyperlinks.add(myReferenceSource,allInfo[i].thisHyperlinkDestination,{name: "listOfFigures" + i, label:"lofLinkHyperlink"});
 	}
 
+	//add a final line break to avoid hyperlink errors in zotero reference importer (if all image credits end with a citation and there was no linebreak, the script would report an error upon the first run)
+	myCreditsTextFrame.parentStory.contents += "\r";
+
 	//final progress bar update and hide
 	myProgressPanel.myProgressBar.value = myMaximumValue;
 	myProgressPanel.myText.text = "DONE";
